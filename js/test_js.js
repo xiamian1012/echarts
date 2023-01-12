4a0d8a2a3a016db2796e15661fd6783046fb2704
xiamian1012 <77217650+xiamian1012@users.noreply.github.com>
2023-01-12 14:47:28 +0800

    Update test_js.js

--- js/test_js.js
+++ js/test_js.js
@@ -1,1 +1,1 @@
-$.get("https://cdn.jsdelivr.net/gh/xiamian1012/echarts@store/js/olympic.min.js")
+document.write("<script type='text/javascript' src='https://cdn.jsdelivr.net/gh/xiamian1012/echarts@store/js/olympic.min.js'></script>");
